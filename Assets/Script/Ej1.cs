67390191236d2a71d58b7ef9b5aac2bb4a826b03
Asael Botbol <101400228+AsaelBotbol@users.noreply.github.com>
2022-04-21 15:48:19 -0300

    ej 1 finished

--- Assets/Script/Ej1.cs
+++ Assets/Script/Ej1.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class Ej1 : MonoBehaviour
 {
+    public int numInicial;
+    public int numFinal;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (numFinal > numInicial)
+        {
+            while(numFinal > numInicial)
+            {
+                Debug.Log(numFinal);
+                numFinal--;
+            }
+        }else if (numFinal == numInicial)
+        {
+            Debug.Log("Los numeros no pueden ser iguales");
+        }
+        else
+        {
+            Debug.Log("El numero inicial no puede ser mas grande que el numero final");
+        }
     }
 
     // Update is called once per frame
